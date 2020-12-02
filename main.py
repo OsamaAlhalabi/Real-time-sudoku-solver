a02e4b57e3811eaed1a8ac491fe942d0af5fe9d5
unknown <eng.osama1998@gmail.com>
2020-12-02 23:03:36 +0200

    corner detection

---
+++ main.py
@@ -0,0 +1,7 @@
+import cv2 as cv
+import numpy as np
+from preprocessing import recognize_sudoku
+if __name__ == "__main__":
+    path = 'E:\\5th Year Subjects\OpenCV\imgs2\\sudoku.jpg'
+    img = cv.imread(path)
+    recognize_sudoku(img)
---
+++ preprocessing.py
@@ -0,0 +1,61 @@
+import cv2 as cv
+import numpy as np
+
+
+def detect_corners(contours, max_iter=200, coefficient=1):
+    while max_iter > 0 and coefficient >= 0:
+        max_iter = max_iter - 1
+        epsilon = coefficient * cv.arcLength(contours, True)
+        poly_approx = cv.approxPolyDP(contours, epsilon, True)
+        hull = cv.convexHull(poly_approx)
+        if len(hull) == 4:
+            return hull
+        else:
+            if len(hull) > 4:
+                coefficient += .01
+            else:
+                coefficient -= .01
+    return None
+
+
+def draw_borders(img, contour, corners):
+    drawing = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+
+    for i in range(len(contour)):
+        color = (255, 0, 0)
+        cv.drawContours(drawing, contour, i, color)
+
+    for i in range(len(corners)):
+        color = (0, 255, 0)
+        cv.drawContours(drawing, corners, i, color)
+
+    cv.dilate(drawing, None)
+    cv.imshow('Contours', drawing)
+
+
+def recognize_sudoku(img):
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    img = cv.GaussianBlur(img, (5, 5), 0)
+    img = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 11, 2)
+    contours, hierarchy = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+    max_area = 0
+    longest_contour = None
+
+    for c in contours:
+        area = cv.contourArea(c)
+        if area > max_area:
+            max_area = area
+            longest_contour = c
+
+    if longest_contour is None:
+        return img
+
+    corners = detect_corners(longest_contour)
+    # draw_borders(img, longest_contour, corners)
+
+    if corners is None:
+        return img
+
+    cv.imshow('res', img)
+    cv.waitKey(0)
