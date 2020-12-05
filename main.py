a71ef987be4ada55f63d1f6934853c12dde7e8d3
Nitro963 <adnankattan9@gmail.com>
2020-12-05 12:41:23 +0200

    retrieve cells method

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import cv2 as cv
-import numpy as np
-from preprocessing import recognize_sudoku
+from preprocessing import recognize_sudoku, filter_and_repair, retrieve_cells
+
 if __name__ == "__main__":
     path = r"C:\Users\Nitro\Desktop\imgs\sudoku.jpg"
     img = cv.imread(path)
     img = recognize_sudoku(img)
-    cv.imshow('output', img)
-    cv.waitKey(0)
+    filtered_img = filter_and_repair(img)
+    cells = retrieve_cells(img, filtered_img)
+    # cv.imshow('output', img)
+    # cv.imshow('output1', fimg)
+    # cv.waitKey(0)
--- preprocessing.py
+++ preprocessing.py
@@ -36,7 +36,7 @@ def draw_borders(img, contour, corners):
 def recognize_sudoku(img):
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     img = cv.GaussianBlur(img, (5, 5), 0)
-    img = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 11, 2)
+    img = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 23, 5)
     img2, contours, hierarchy = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
     areas = np.array(list(map(cv.contourArea, contours)))
@@ -58,3 +58,53 @@ def recognize_sudoku(img):
     perspective_transformed_matrix = cv.getPerspectiveTransform(rect, mat)
     warp = cv.warpPerspective(img, perspective_transformed_matrix, (w, h))
     return warp
+
+
+def filter_and_repair(img):
+    img, contours, hierarchy = cv.findContours(img.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+    for c in contours:
+        area = cv.contourArea(c)
+        if area < 1000:
+            cv.drawContours(img, [c], -1, (0, 0, 0), -1)
+
+    # Fix horizontal and vertical lines
+    vertical_kernel = cv.getStructuringElement(cv.MORPH_RECT, (1, 5))
+    img = cv.morphologyEx(img, cv.MORPH_CLOSE, vertical_kernel, iterations=7)
+
+    horizontal_kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 1))
+    img = cv.morphologyEx(img, cv.MORPH_CLOSE, horizontal_kernel, iterations=5)
+
+    return img
+
+
+def retrieve_cells(img, thresh):
+    # Sort by top to bottom and each row by left to right
+    invert = 255 - thresh
+    _, contours, _ = cv.findContours(invert, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    contours, _ = calculation.sort_contours(contours, method="top-to-bottom")
+
+    sudoku_rows = []
+    row = []
+    cells = []
+    for (i, c) in enumerate(contours, 1):
+        area = cv.contourArea(c)
+        if area < 3000:
+            row.append(c)
+            if i % 9 == 0:
+                contours, bounding_boxes = calculation.sort_contours(row, method="left-to-right")
+                sudoku_rows.append(contours)
+                row.clear()
+                cells.extend([img[y: y + h, x: x + w] for (x, y, w, h) in bounding_boxes])
+
+    # Iterate through each box
+    for row in sudoku_rows:
+        for c in row:
+            mask = np.zeros(img.shape, dtype=np.uint8)
+            cv.drawContours(mask, [c], -1, (255, 255, 255), -1)
+            result = cv.bitwise_and(img, mask)
+            result[mask == 0] = 255
+            cv.imshow('result', result)
+            cv.waitKey(175)
+
+    return cells
